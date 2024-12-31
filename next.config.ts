51aefc90106836e388044f85d170e97be2793997
Lee-Dongwook <dlehddnr0713@gmail.com>
2024-12-31 12:43:55 +0900

    chore : update dependencies and default settings

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,60 @@
 import type { NextConfig } from "next";
 
+import { redirects } from "./redirect";
+
+const ContentSecurityPolicy = `
+  object-src 'none';
+  base-uri 'self';
+  frame-ancestors 'self';
+  manifest-src 'self';
+  report-to default;
+`;
+
+const securityHeaders = [
+  {
+    key: "X-DNS-Prefetch-Control",
+    value: "on",
+  },
+  {
+    key: "Strict-Transport-Security",
+    value: "max-age=63072000; includeSubDomains; preload",
+  },
+  {
+    key: "X-XSS-Protection",
+    value: "1; mode=block",
+  },
+  {
+    key: "X-Content-Type-Options",
+    value: "nosniff",
+  },
+  {
+    key: "Referrer-Policy",
+    value: "no-referrer-when-downgrade",
+  },
+  {
+    key: "Permissions-Policy",
+    value: `accelerometer=(), camera=(), gyroscope=(), microphone=(), usb=()`,
+  },
+  {
+    key: "Content-Security-Policy",
+    value: ContentSecurityPolicy.replace(/\n/g, ""),
+  },
+];
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  poweredByHeader: false,
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: securityHeaders,
+      },
+    ];
+  },
+  async redirects() {
+    return redirects;
+  },
+  reactStrictMode: true,
 };
 
 export default nextConfig;
